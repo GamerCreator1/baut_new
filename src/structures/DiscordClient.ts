7edf7dbd9df70a05c9ffcb295a056a63db4b919c
Josiah <josiahrose@josiahrose.me>
2022-05-24 20:22:02 -0400

    Fixed dev commandline

--- src/structures/DiscordClient.ts
+++ src/structures/DiscordClient.ts
@@ -1,10 +1,9 @@
 import { Client, ClientOptions } from 'discord.js';
 import { BotOptions } from '@utils/types';
-import { prisma } from '@providers/prisma';
+import { prisma } from '../providers/prisma';
 
 import Registry from '@classes/Registry';
 import config from '../config';
-
 export default class DiscordClient extends Client {
     public readonly config = config;
     public readonly registry = new Registry(this);
