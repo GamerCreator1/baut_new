5832b7a8a3946e1ad393578f8ee8c4049634f911
Neesh774 <Kanishq0106@gmail.com>
2022-08-23 14:37:42 -0400

    add role on sign up

--- src/embeds/hacks.ts
+++ src/embeds/hacks.ts
@@ -13,11 +13,13 @@ import {
     ButtonInteraction,
     ComponentType,
     Message,
+    GuildMember,
 } from "discord.js";
 
 import DiscordClient from "@structures/DiscordClient";
 import Embed from "@structures/Embed";
 import { HacksParticipant } from "@utils/interfaces";
+import Logger from "@classes/Logger";
 
 export default class BuilderHacksEmbed extends Embed {
     private static sessions: Collection<string, string> = new Collection();
@@ -61,56 +63,94 @@ export default class BuilderHacksEmbed extends Embed {
     async onInteraction(interaction: Interaction<CacheType>, client: DiscordClient): Promise<void> {
         if (interaction.isButton() && interaction.customId == "register") {
             await interaction.deferReply({ ephemeral: true });
-            const channel = interaction.channel;
-            const hacksParticipants = await client.db.hacksParticipants.findMany();
-
-            if (channel.type == ChannelType.GuildText) {
-                if (BuilderHacksEmbed.sessions.has(channel.id)) {
-                    await interaction.editReply("You are already registered for this session.");
-                    return;
-                } else if (hacksParticipants.some(p => p.userIds.includes(interaction.user.id))) {
-                    await interaction.editReply("You are already registered for this session. If you want to edit your team or audit times, please contact the organizers.");
-                    return;
-                }
-
-                const thread = await channel.threads.create({
-                    name: "BuilderHacks Registration",
-                    autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
-                    reason: `${interaction.user.tag} started registering for BuilderHacks.`,
-                    type: client.config.prod ? ChannelType.GuildPrivateThread : ChannelType.GuildPublicThread,
-                });
-                BuilderHacksEmbed.sessions.set(interaction.user.id, thread.id);
-                await interaction.editReply(`Started a thread for you to register your team: <#${thread.id}>`);
-
-                let sub = {} as HacksParticipant;
-
-                const initMessage = await thread.send({
-                    content: interaction.user.toString(),
-                    embeds: [
-                        new EmbedBuilder().setDescription(
-                            "Send a message pinging your other teammate, if you have one. If you don't, send `none`. Press the red button below to cancel at any time during this process."
-                        ),
-                    ],
-                    components: [new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setLabel("Cancel").setCustomId("cancel").setStyle(ButtonStyle.Danger))],
-                });
-
-                const cancelFilter = (i: ButtonInteraction) => i.customId == "cancel" && i.user.id == interaction.user.id;
-                const msgFilter = (m: Message) => m.author.id == interaction.user.id;
-                const cancelCollector = initMessage.createMessageComponentCollector({ filter: cancelFilter, componentType: ComponentType.Button, max: 1 });
-                cancelCollector.on("collect", async () => {
-                    await interaction.editReply("Cancelled registration.");
-                    BuilderHacksEmbed.sessions.delete(interaction.user.id);
-                    await thread.delete();
-                    this.cancelled = true;
-                });
-
-                if (this.cancelled) return;
-
-                await thread.awaitMessages({ filter: msgFilter, max: 1 }).then(c => {
-                    const userIds = c.first().mentions.users.map(u => u.id);
-                    sub.userIds = [interaction.user.id, ...userIds];
-                });
+            try {
+                const role = await interaction.guild.roles.fetch(process.env.BUILDERHACKS_ROLE);
+                await (interaction.member as GuildMember).roles.add(role);
+            } catch (e) {
+                Logger.log("ERROR", e.stack);
             }
+            // const channel = interaction.channel;
+            // const hacksParticipants = await client.db.hacksParticipants.findMany();
+
+            // if (channel.type == ChannelType.GuildText) {
+            //     if (BuilderHacksEmbed.sessions.has(channel.id)) {
+            //         await interaction.editReply("You are already registered for this session.");
+            //         return;
+            //     } else if (hacksParticipants.some(p => p.userIds.includes(interaction.user.id))) {
+            //         await interaction.editReply("You are already registered for this session. If you want to edit your team or audit times, please contact the organizers.");
+            //         return;
+            //     }
+
+            //     const thread = await channel.threads.create({
+            //         name: "BuilderHacks Registration",
+            //         autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
+            //         reason: `${interaction.user.tag} started registering for BuilderHacks.`,
+            //         type: client.config.prod ? ChannelType.GuildPrivateThread : ChannelType.GuildPublicThread,
+            //     });
+            //     BuilderHacksEmbed.sessions.set(interaction.user.id, thread.id);
+            //     await interaction.editReply(`Started a thread for you to register your team: <#${thread.id}>`);
+
+            //     let sub = {} as HacksParticipant;
+
+            //     const initMessage = await thread.send({
+            //         content: interaction.user.toString(),
+            //         embeds: [
+            //             new EmbedBuilder().setDescription(
+            //                 "Send a message pinging your other teammate, if you have one. If you don't, send `none`. Press the red button below to cancel at any time during this process."
+            //             ),
+            //         ],
+            //         components: [new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setLabel("Cancel").setCustomId("cancel").setStyle(ButtonStyle.Danger))],
+            //     });
+
+            //     const cancelFilter = (i: ButtonInteraction) => i.customId == "cancel" && i.user.id == interaction.user.id;
+            //     const msgFilter = (m: Message) => m.author.id == interaction.user.id;
+            //     const cancelCollector = initMessage.createMessageComponentCollector({ filter: cancelFilter, componentType: ComponentType.Button, max: 1 });
+            //     cancelCollector.on("collect", async () => {
+            //         await interaction.editReply("Cancelled registration.");
+            //         BuilderHacksEmbed.sessions.delete(interaction.user.id);
+            //         await thread.delete();
+            //         this.cancelled = true;
+            //     });
+
+            //     if (this.cancelled) return;
+
+            //     await thread
+            //         .awaitMessages({ filter: msgFilter, max: 1 })
+            //         .then(async c => {
+            //             if (this.cancelled) return;
+            //             const userIds = c.first().mentions.users.map(u => u.id);
+            //             sub.userIds = [interaction.user.id, ...userIds];
+            //             userIds.forEach(async u => {
+            //                 if (hacksParticipants.some(p => p.userIds.includes(u))) {
+            //                     await c.first().reply(`<@${u} is already registered for a team. Please contact the organizers to edit teams. Exiting process now...`);
+            //                     BuilderHacksEmbed.sessions.delete(interaction.user.id);
+            //                     await thread.delete();
+            //                     this.cancelled = true;
+            //                     return;
+            //                 }
+            //             });
+            //             await c.first().reply("Got it! Next, send a message with your team name! Please make sure it's less than 20 characters.");
+            //         })
+            //         .catch(() => {
+            //             sub.userIds = [interaction.user.id];
+            //         });
+            //     if (this.cancelled) return;
+            //     const teamNameFilter = (m: Message) => m.author.id == interaction.user.id && m.content.length < 20;
+            //     await thread.awaitMessages({ filter: teamNameFilter, maxProcessed: 1 }).then(async c => {
+            //         if (this.cancelled) return;
+            //         sub.teamName = c.first().content;
+            //         await c.first().reply("Perfect! Adding your team to the database...");
+            //     });
+            //     if (this.cancelled) return;
+
+            //     await client.db.hacksParticipants.create({
+            //         data: sub,
+            //     });
+
+            //     await interaction.editReply("Successfully registered for BuilderHacks!");
+            //     await thread.delete(`Completed registration for ${interaction.user.tag}`);
+            //     BuilderHacksEmbed.sessions.delete(interaction.user.id);
+            // }
         }
     }
 }
