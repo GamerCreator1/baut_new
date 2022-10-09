aaf96c5c7558e7fcfc58ccad50f19c5812edfcda
GamerCreator1 <thegamercreator7@gmail.com>
2022-10-09 14:37:34 -0400

    Remove comment out code

--- src/embeds/reactionRoles.ts
+++ src/embeds/reactionRoles.ts
@@ -2,7 +2,6 @@ import {
     CacheType,
     GuildMember,
     Interaction,
-    Message,
     ActionRowBuilder,
     ButtonBuilder,
     EmbedBuilder,
@@ -78,62 +77,5 @@ export default class ReactionRolesEmbed extends Embed {
             });
 
         }
-        //     if (interaction.isButton()) {
-        //         await interaction.deferReply({ ephemeral: true });
-        //         const roleObj = ReactionRoles.find(role => "roles/option/" + role.customId === interaction.customId);
-        //         const roleObjRoles = roleObj.options.map(r => r.role);
-        //         const user = interaction.member as GuildMember;
-        //         if (!roleObj) return;
-        //         const embed = {
-        //             title: roleObj.embed.title,
-        //             description: roleObj.embed.description,
-        //             footer: {
-        //                 text: roleObj.type,
-        //             },
-        //         };
-        //         const message = (await interaction.editReply({
-        //             embeds: [embed],
-        //             components: [selectMenu],
-        //         })) as Message;
-
-        //         const roleSelectorFilter = (i: SelectMenuInteraction) => {
-        //             return i.user.id == user.id && i.customId.startsWith("roles/selection/");
-        //         };
-        //         const collector = message.createMessageComponentCollector({ componentType: ComponentType.SelectMenu, time: 60000, filter: roleSelectorFilter });
-        //         collector.on("collect", async (select: SelectMenuInteraction) => {
-        //             await select.deferReply({ ephemeral: true });
-        //             if (roleObj.type == "Multi Select") {
-        //                 const options = select.values;
-        //                 const removed = user.roles.cache.filter(role => !options.includes(role.id) && roleObjRoles.includes(role.id));
-        //                 removed.forEach(async role => await user.roles.remove(role));
-        //                 const added = options.filter(option => !user.roles.cache.has(option) && roleObjRoles.includes(option));
-        //                 added.forEach(async role => await user.roles.add(role));
-
-        //                 await select.editReply(`:heavy_minus_sign: ${removed.size > 0 ? removed.map(r => r.toString()).join(", ") : "None"}\n:heavy_plus_sign: ${added.length > 0 ? `<@&${added.map(r => r.toString()).join(">, <@&")}>` : "None"}`);
-        //             } else {
-        //                 let removed;
-        //                 roleObj.options.forEach(async option => {
-        //                     if (option.role == select.values[0]) {
-        //                         await user.roles.add(option.role);
-        //                     } else if (user.roles.cache.has(option.role)) {
-        //                         removed = option.role;
-        //                         await user.roles.remove(option.role);
-        //                     }
-        //                 });
-        //                 if (removed) {
-        //                     await select.editReply(`:heavy_minus_sign: ${removed}\n:heavy_plus_sign: ${select.values[0]}`);
-        //                 } else {
-        //                     await select.editReply(`:heavy_plus_sign: ${select.values[0]}`);
-        //                 }
-        //             }
-        //         });
-        //         collector.on("end", (collected: SelectMenuInteraction[], reason: string) => {
-        //             selectMenu.components[0].setDisabled(true);
-        //             interaction.editReply({
-        //                 content: "Session ended.",
-        //                 components: [selectMenu],
-        //             });
-        //         });
-        //     }
     }
 }
